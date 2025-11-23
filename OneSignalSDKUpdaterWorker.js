613cd7acf4894673a817b25b6ec5638eac8bb49b
Juan Jesús Silvestre Chávez <134343358+TYisusK@users.noreply.github.com>
2025-11-23 17:31:10 -0600

    Update import script for OneSignal SDK

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,3 +1,2 @@
 // OneSignal updater worker (root scope)
-/* Nota: Mantener este archivo exactamente en la RAÍZ del dominio */
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
